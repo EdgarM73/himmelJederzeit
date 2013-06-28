7ab64fdff19e25de1b217199a97e0cef5f54fbcc
EdgarM73 <Edgar.Miller@gmail.com>
2013-06-28 20:57:39 +0200

    added automatic installation per ftp

---
+++ update.bat
@@ -0,0 +1,15 @@
+cls
+
+@ECHO OFF
+
+ECHO Install himmelJederzeit Plugin
+
+ECHO Nur fuer Coolstream NG-RETURN Image
+
+ECHO -----------------------------------------------------------------------------------------------------------
+
+Pause
+
+ftp -s:config.txt
+
+pause
