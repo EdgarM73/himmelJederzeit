de1539713691ff635935d2270ae05edc6d960c35
EdgarM73 <Edgar.Miller@gmail.com>
2013-06-29 20:03:22 +0200

    changed windows newline for update.bat

--- var/plugins/himmelJederzeit.sh
+++ var/plugins/himmelJederzeit.sh
@@ -1,3 +1,40 @@
+# Copyright (C) 2012-2013  Erdal Akkaya, erdal@akkaya.info
+#
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+
+# Related files:
+#	Configuration:		/var/tuxbox/config/pr-auto-timer.conf
+#	Timer rules:	 	/var/tuxbox/config/pr-auto-timer.rules
+#	Neutrino plugin:	/lib/tuxbox/plugins/pr-auto-timer.{so,cfg}
+
+# Todo:
+#
+#	-show pr-auto-timer as a script plugin in neutrino
+#	-simulate Sky Anytime
+
+# Changelog:
+#
+# 0.17
+#	-accept day groups "Weekday" and "Weekend"
+#	-small code optimizations
+#	-accept uppercase flags only
+#	-Add possible exlusion expressions
+#		FOX HD;*;LOST,!Special
+#
+
 platform=`uname`
 
 pwd=`pwd`
