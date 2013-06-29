cf112de766cc79f9f5b15b0f10775617e92dacd7
EdgarM73 <Edgar.Miller@gmail.com>
2013-06-30 00:13:07 +0200

    again ;) bugfixed deactivating

--- lib/tuxbox/plugins/InitialisierehimmelJederzeit.sh
+++ lib/tuxbox/plugins/InitialisierehimmelJederzeit.sh
@@ -2,4 +2,4 @@
 echo "Die Grundstruktur f&uuml;r himmelJederzeit wird erzeugt"
 cd /var/plugins;./himmelJederzeit.sh initGUI
 echo "Dieses Script wird deaktiviert"
-sed -e 's#hide=0#hide=1#' ./InitialisierehimmelJederzeit.cfg > tmp && mv tmp ./InitialisierehimmelJederzeit.cfg
+sed -e 's#hide=0#hide=1#' /lib/tuxbox/plugins/InitialisierehimmelJederzeit.cfg > /lib/tuxbox/plugins/tmp && mv /lib/tuxbox/plugins/tmp /lib/tuxbox/plugins/InitialisierehimmelJederzeit.cfg
