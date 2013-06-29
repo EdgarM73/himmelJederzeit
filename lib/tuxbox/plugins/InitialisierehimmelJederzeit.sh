09d64704e960fc34d21e7f7c2eb0d2ce218d98fa
EdgarM73 <Edgar.Miller@gmail.com>
2013-06-30 00:26:41 +0200

    added complete run to initialiseGUI Plugin

--- lib/tuxbox/plugins/InitialisierehimmelJederzeit.sh
+++ lib/tuxbox/plugins/InitialisierehimmelJederzeit.sh
@@ -3,3 +3,4 @@ echo "Die Grundstruktur f&uuml;r himmelJederzeit wird erzeugt"
 cd /var/plugins;./himmelJederzeit.sh initGUI
 echo "Dieses Script wird deaktiviert"
 sed -e 's#hide=0#hide=1#' /lib/tuxbox/plugins/InitialisierehimmelJederzeit.cfg > /lib/tuxbox/plugins/tmp && mv /lib/tuxbox/plugins/tmp /lib/tuxbox/plugins/InitialisierehimmelJederzeit.cfg
+cd /var/plugins;./himmelJederzeit.sh full
