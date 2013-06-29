ad4388278a79e8065b8cda2a989e715f4c10acbc
EdgarM73 <Edgar.Miller@gmail.com>
2013-06-29 23:54:49 +0200

    remove gelaufen file

--- lib/tuxbox/plugins/InitialisierehimmelJederzeit.sh
+++ lib/tuxbox/plugins/InitialisierehimmelJederzeit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Die Grundstruktur f&uuml;r himmelJederzeit wird erzeugt"
-sed -e 's#hide=0#hide=1#' ./InitialisierehimmelJederzeit.cfg > tmp && mv tmp ./InitialisierehimmelJederzeit.cfg
 cd /var/plugins;./himmelJederzeit.sh initGUI
 echo "Dieses Script wird deaktiviert"
+sed -e 's#hide=0#hide=1#' ./InitialisierehimmelJederzeit.cfg > tmp && mv tmp ./InitialisierehimmelJederzeit.cfg
