64767e8aa528419a5abb0a7055eecde443e7ec74
EdgarM73 <Edgar.Miller@gmail.com>
2013-07-05 13:06:04 +0200

    rename of transfer files, as there is a need for two runs: first deployment of .bin file second, after plugin install via gui, deployment of the config file

---
+++ setup/transferConfig.bat
@@ -0,0 +1,20 @@
+cls
+
+@ECHO OFF
+
+ECHO Install himmelJederzeit Plugin
+
+ECHO Nur fuer Coolstream NG-RETURN Image
+
+ECHO -----------------------------------------------------------------------------------------------------------
+
+ECHO -----------------------------------------------------------------------------------------------------------
+ECHO Ist die Konfiguration angepasst? Falls nicht, bitte Fenster schließen
+ECHO var/tuxbox/config/jederzeit/himmelJederzeit.cfg.template
+ECHO -----------------------------------------------------------------------------------------------------------
+
+Pause
+
+ftp -s:transferConfig.txt
+
+pause
