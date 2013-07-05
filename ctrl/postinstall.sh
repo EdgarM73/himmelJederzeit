28618e42f621fc9a780f68ba1207906bbe013e5a
EdgarM73 <Edgar.Miller@gmail.com>
2013-07-05 22:52:36 +0200

    no reboot after install

--- ctrl/postinstall.sh
+++ ctrl/postinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-/bin/dt -t"Rebooting..."
-sleep 3
-/bin/sync
-sleep 2
-/sbin/reboot
+#/bin/dt -t"Rebooting..."
+#sleep 3
+#/bin/sync
+#sleep 2
+#/sbin/reboot
+cd /var/plugins && ./himmelJederzeit.sh && ./himmelJederzeit.sh Stand
+echo "Erste Ausführung erledigt"
