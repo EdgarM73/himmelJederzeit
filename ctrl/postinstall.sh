2071d59fc29a2d220941134209b7509d79d80838
EdgarM73 <Edgar.Miller@gmail.com>
2013-07-13 00:28:59 +0200

    after installation .bin file will be removed

--- ctrl/postinstall.sh
+++ ctrl/postinstall.sh
@@ -2,3 +2,4 @@
 touch /var/tuxbox/config/jederzeit/gelaufen
 wget -q -O - "http://localhost/control/message?popup=himmelJEderzeit%20wurde%20installiert."
 wget -q -O - "http://localhost/control/reloadplugins"
+rm /tmp/himmelJederzeit.bin
